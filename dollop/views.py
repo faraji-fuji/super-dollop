44d4e1d0513799f3609aae1d6ec5e305538fb02c
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-11 10:35:17 +0300

    feat: entry view

--- dollop/views.py
+++ dollop/views.py
@@ -4,7 +4,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .serializers import UserSerializer, GetUserSerializer
+from .serializers import UserSerializer, GetUserSerializer, EntrySerializer, GetEntrySerializer
+from .models import Entry
 
 
 @api_view(['GET', 'POST'])
@@ -31,3 +32,26 @@ def user_list(request):
 def logout(request):
     request.auth.delete()
     return Response({"detail": "logged out successfully"}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'POST'])
+@authentication_classes([TokenAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def entry_list(request):
+    """
+    List entries or create a new entry
+    """
+    if request.method == 'GET':
+        entries = Entry.objects.filter(user_id=request.user.id)
+        serializer = GetEntrySerializer(entries, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = EntrySerializer(data=request.data)
+        if serializer.is_valid():
+            new_entry = serializer.save()
+            entry = Entry.objects.create(user_id=request.user.id,
+                                         value=new_entry['value'])
+            serializer = GetEntrySerializer(entry)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
