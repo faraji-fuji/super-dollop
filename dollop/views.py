367c903bd437bab56e0aec07db2ba3d2c921f68f
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-11 10:03:39 +0300

    feat: user_list view

--- dollop/views.py
+++ dollop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer, GetUserSerializer
 
-# Create your views here.
+
+@api_view('GET', 'POST')
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = GetUserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            user = User.objects.create(username=new_user['username'],
+                                       password=new_user['password'])
+            serializer = GetUserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
