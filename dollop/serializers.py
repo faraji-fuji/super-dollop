8c16ce483bc469b99cb107bad47ec46df7442df3
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-11 10:27:11 +0300

    feat: entry serializers

--- dollop/serializers.py
+++ dollop/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Entry
 
 
 class UserSerializer(serializers.Serializer):
@@ -14,3 +15,16 @@ class GetUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class EntrySerializer(serializers.Serializer):
+    value = serializers.CharField()
+
+    def create(self, validated_data):
+        return validated_data
+
+
+class GetEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        field = "__all__"
