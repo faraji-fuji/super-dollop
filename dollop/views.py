69cbbe5e600a8e7dc76e3000d665932c82ff15fc
Faraji Ombonya <farajiombonya@gmail.com>
2023-11-11 10:20:16 +0300

    feat: logout view

--- dollop/views.py
+++ dollop/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import UserSerializer, GetUserSerializer
 
@@ -21,3 +23,11 @@ def user_list(request):
             serializer = GetUserSerializer(user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.auth.delete()
+    return Response({"detail": "logged out successfully"}, status=status.HTTP_200_OK)
